3275c04b80c09a95d0b6e66286623c4003bc764b
Shreshth Gupta <66182658+shoutingmonkey@users.noreply.github.com>
2022-03-14 18:57:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import { React, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
-=======
 import { React } from "react";
->>>>>>> completed assignment
+
 
 import "./App.css";
 import Login from "./Components/Login";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <div className="App">
-        <Login />
-      </div>
-    </BrowserRouter>
-=======
     <div className="App">
       <Login />
     </div>
->>>>>>> completed assignment
   );
 }
 
